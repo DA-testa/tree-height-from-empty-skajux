de75e54cca2c351eb6bbe182c24ad33df5bafbc2
221RDB223 <125821576+skajux@users.noreply.github.com>
2023-03-05 22:59:19 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,29 +1,23 @@
-# python3
-
 import sys
-import threading
+
+sys.setrecursionlimit(10**6)
 
 def compute_height(n, parents):
-    children = [[] for i in range(n)]
-    root_index = -1
+    tree = {}
     for i in range(n):
-        parent_index = parents[i]
-        if parent_index == -1:
-            root_index = i
+        if parents[i] == -1:
+            root = i
         else:
-            children[parent_index].append(i)
-    def compute_subtree_height(node_index):
-        if not children[node_index]:
-            return 1
+            if parents[i] not in tree:
+                tree[parents[i]] = []
+            tree[parents[i]].append(i)
+    def compute_height_helper(node):
+        if node not in tree:
+            return 0
         else:
-            child_heights = [compute_subtree_height(child_index) for child_index in children[node_index]]
-            return max(child_heights) + 1
-    return compute_subtree_height(root_index)
-
-def main():
-    n = int(input())
-    parents = list(map(int, input().split()))
-    print(compute_height(n, parents))
-sys.setrecursionlimit(10**7)
-threading.stack_size(2**27)
-threading.Thread(target=main).start()
+            heights = [compute_height_helper(child) for child in tree[node]]
+            return max(heights) + 1
+    return compute_height_helper(root)
+n = int(input())
+parents = list(map(int, input().split()))
+print(compute_height(n, parents))
