c4ec9a2bb285aeff26b8322cdb5ec9a6ebf9d809
221RDB223 <125821576+skajux@users.noreply.github.com>
2023-03-05 22:54:05 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,23 +2,28 @@
 
 import sys
 import threading
-import numpy
+
 def compute_height(n, parents):
     children = [[] for i in range(n)]
-    root_index = parents.index(-1)
+    root_index = -1
     for i in range(n):
-        if parents[i] != -1:
-            children[parents[i]].append(i)
+        parent_index = parents[i]
+        if parent_index == -1:
+            root_index = i
+        else:
+            children[parent_index].append(i)
     def compute_subtree_height(node_index):
         if not children[node_index]:
             return 1
-        return 1 + max(compute_subtree_height(child_index) for child_index in children[node_index])
+        else:
+            child_heights = [compute_subtree_height(child_index) for child_index in children[node_index]]
+            return max(child_heights) + 1
     return compute_subtree_height(root_index)
+
 def main():
     n = int(input())
     parents = list(map(int, input().split()))
     print(compute_height(n, parents))
-sys.setrecursionlimit(10**7)  
-threading.stack_size(2**27)   
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
-main()
